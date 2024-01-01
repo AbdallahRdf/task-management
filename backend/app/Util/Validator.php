3eff18d4a94f12dea51010a4224a2659c5185466
abdallah <abdallahradfi@gmail.com>
2024-01-01 18:41:02 +0100

    created first controller

---
+++ backend/app/Util/Validator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Backend\App\Util;
+
+class Validator
+{
+    // validate the name
+    public static function isNameValid($name)
+    {
+        return preg_match("/[a-zA-Z]+(\s*([a-zA-Z])*)*$/", $name); // This pattern is designed to match words consisting of alphabetical characters
+    }
+
+    // validate the name
+    public static function isStrValid($str)
+    {
+        return preg_match("/[a-zA-Z0-9]+(\s*([a-zA-Z0-9])*)*$/", $str); // This pattern is designed to match words consisting of alphabetical characters
+    }
+
+    // validate the password
+    public static function isPasswordValid($password)
+    {
+        if (strlen($password) < 8) {
+            return "Password must be at least 8 characters long.";
+        }
+        if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9_\-!#]*$/", $password)) {
+            return "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character (_-!#).";
+        }
+        return "valid";
+    }
+}
--- backend/controllers/auth.php
+++ backend/controllers/auth.php
@@ -1,80 +1,37 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+namespace Backend\Controllers;
 
-require_once "../util/functions.php";
-require_once "../models/User.php";
+use Backend\App\Models\User;
 
-session_start();
-
-function user_is_valid($user_credentials)
+class Auth
 {
-    $_SESSION["user"] = $user_credentials;
-    header("location: ../resources/views/home.php");
-    die();
-}
-
-if($_SERVER["REQUEST_METHOD"] === "POST")
-{
-    $email = trim($_POST["email"]);
-    $password = $_POST["password"];
-
-    if(isset($_POST["name"])) // handles sign up process
+    public static function sign_up($first_name, $last_name, $email, $password): array|false
     {
-        $name = trim($_POST["name"]);
-
-        $ERRORS = [];
-        $OLD = [];
-
-        if(!isNameValid($name))
+        if(!empty(User::get([User::EMAIL => $email])))
         {
-            $ERRORS["name_error_message"] = "Invalid Name";
+            return false;
         }
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL) || count(User::get_user_credentials($email)) > 0)
-        {
-            $ERRORS["email_error_message"] = "Invalid Email";
-        }
-        $message = isPasswordValid($password);
-        if($message !== "valid")
-        {
-            $ERRORS["password_error_message"] = $message;
-        }
-        
-        if(!empty($ERRORS))
-        {
-            $OLD["old_name"] = $name;
-            $OLD["old_email"] = $email;
 
-            $_SESSION["old"] = $OLD;
-            $_SESSION["signup_errors"] = $ERRORS;
-            header("location: ../resources/views/signup.php");
-            die();
-        }
-        $hash = password_hash($password, PASSWORD_DEFAULT);
-        User::create($name, $email, $hash);
+        User::create([
+            User::FIRST_NAME => $first_name,
+            User::LAST_NAME => $last_name,
+            User::EMAIL => $email,
+            User::PASSWORD => password_hash($password, PASSWORD_DEFAULT),
+            User::CONNECTED => true
+        ]);
 
-        user_is_valid(User::get_user_credentials($email)[0]);
-    }
-    else if(isset($_POST["email"])) // handles login process
+        return User::get([User::EMAIL => $email]);
+    }   
+
+    public static function log_in($email, $password): array|false
     {
-        $users_credentials = User::get_user_credentials($email);
-        $is_password_correct = password_verify($password, $users_credentials[0]["password"]);
-        
-        // if the email is not valid, there is no account with the email, or the password is incorrect then:
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL) || count($users_credentials) <= 0 || !$is_password_correct) 
+        $user_credintials = User::get([User::EMAIL => $email]);
+
+        if(empty($user_credintials) || !password_verify($password, $user_credintials["password"]))
         {
-            $_SESSION["old"] = $email;
-            header("location: ../resources/views/login.php");
-            die();
+            return false;
         }
-        user_is_valid($users_credentials[0]);
+        return $user_credintials;
     }
 }
-if(isset($_SESSION["user"]))
-{
-    unset($_SESSION);
-    session_destroy();
-}
-header("location: ../index.html");
-die();
